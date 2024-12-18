734571e34fc8e15db4d919e930c71f4ceaca54df
Nostrademous <nostrademous@hotmail.com>
2024-12-18 16:36:11 -0500

    WIP - Spirit

--- src/Data/Global.lua
+++ src/Data/Global.lua
@@ -63,6 +63,7 @@ colorCodes.INTELLIGENCE = colorCodes.WITCH
 
 colorCodes.LIFE = colorCodes.MARAUDER
 colorCodes.MANA = colorCodes.WITCH
+colorCodes.SPIRIT = colorCodes.SOURCE
 colorCodes.ES = colorCodes.SOURCE
 colorCodes.WARD = colorCodes.RARE
 colorCodes.ARMOUR = colorCodes.NORMAL
