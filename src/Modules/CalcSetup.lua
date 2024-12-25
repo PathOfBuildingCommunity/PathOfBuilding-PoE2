dedd312714195e783e62935bd9fffb44511b1227
Nostrademous <nostrademous@hotmail.com>
2024-12-25 10:56:20 -0500

    fix for Widowhail

--- src/Modules/CalcSetup.lua
+++ src/Modules/CalcSetup.lua
@@ -994,11 +994,6 @@ function calcs.initEnv(build, mode, override, specEnv)
 					local widowHailMod=(1 + (items["Weapon 1"].baseModList:Sum("INC", nil, "EffectOfBonusesFromQuiver") or 100) / 100)
 					scale = scale * widowHailMod
 					env.modDB:NewMod("WidowHailMultiplier", "BASE", widowHailMod, "Widowhail")
-					local combinedList = new("ModList")
-					for _, mod in ipairs(srcList) do
-						combinedList:MergeMod(mod)
-					end
-					env.itemModDB:ScaleAddList(combinedList, scale)
 				elseif env.modDB.multipliers["CorruptedMagicJewelEffect"] and item.type == "Jewel" and item.rarity == "MAGIC" and item.corrupted and slot.nodeId and item.base.subType ~= "Charm" then
 					scale = scale + env.modDB.multipliers["CorruptedMagicJewelEffect"]
 					local combinedList = new("ModList")
