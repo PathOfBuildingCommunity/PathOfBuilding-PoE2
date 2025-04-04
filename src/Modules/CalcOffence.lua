e2be1b5c60ab28a5e27a09afbc0e4cc59b019383
lucs66 <lucainator@gmail.com>
2025-04-03 23:58:59 -0700

    pinned enemies cant evade

--- src/Modules/CalcOffence.lua
+++ src/Modules/CalcOffence.lua
@@ -2181,7 +2181,7 @@ function calcs.offence(env, actor, activeSkill)
 				}
 			end
 		end
-		if not isAttack or skillModList:Flag(cfg, "CannotBeEvaded") or skillData.cannotBeEvaded or (env.mode_effective and enemyDB:Flag(nil, "CannotEvade")) then
+		if not isAttack or skillModList:Flag(cfg, "CannotBeEvaded") or skillData.cannotBeEvaded or (env.mode_effective and enemyDB:Flag(nil, "CannotEvade")) or enemyDB:Flag(nil, "Condition:Pinned") then
 			output.AccuracyHitChance = 100
 		else
 			local enemyEvasion = m_max(round(calcLib.val(enemyDB, "Evasion")), 0)
