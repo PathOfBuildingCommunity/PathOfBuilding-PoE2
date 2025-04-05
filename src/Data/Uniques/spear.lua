8145fa22a0be713c64ea217f59ad23c11e161430
Blitz54 <football_and_beer@msn.com>
2025-04-04 23:02:37 -0500

    Chainsting

--- src/Data/Uniques/spear.lua
+++ src/Data/Uniques/spear.lua
@@ -10,5 +10,15 @@ Adds 1 to (80-120) Lightning Damage
 (15-30)% increased Attack Speed
 (50-100)% increased chance to Shock
 Rolls only the minimum or maximum Damage value for each Damage Type
+]],[[
+Chainsting
+Hunting Spear
+Implicits: 1
+(15-25)% chance to Maim on Hit
+Adds (13-17) to (22-28) Physical Damage
++(100-150) to Accuracy Rating
+10% increased Attack Speed
+All Damage from Hits with this Weapon Contributes to Pin Buildup
+(20-30)% increased Projectile Speed with this Weapon
 ]],
 }
--- src/Export/Uniques/spear.lua
+++ src/Export/Uniques/spear.lua
@@ -10,5 +10,15 @@ UniqueLocalAddedLightningDamage1
 UniqueLocalIncreasedAttackSpeed3
 UniqueShockChanceIncrease1
 UniqueLocalAlwaysMinimumOrMaximum1
+]],[[
+Chainsting
+Hunting Spear
+Implicits: 1
+SpearImplicitLocalChanceToMaim1
+UniqueLocalAddedPhysicalDamage19
+UniqueLocalIncreasedAccuracy7
+UniqueLocalIncreasedAttackSpeed1
+UniqueLocalAllDamageCanPin1
+UniqueLocalIncreasedProjectileSpeed1
 ]],
 }
