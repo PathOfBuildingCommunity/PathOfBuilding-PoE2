86804aedd18276668faf3fbb802cab8510b2af1c
Blitz54 <football_and_beer@msn.com>
2025-06-04 07:33:51 -0500

    Highlight selected column, and sort by clicking column header

--- src/Export/Classes/RowListControl.lua
+++ src/Export/Classes/RowListControl.lua
@@ -44,13 +44,18 @@ end
 
 function RowListClass:BuildColumns()
 	wipeTable(self.colList)
-	self.colList[1] = { width = 50, label = "#", font = "FIXED" }
+	self.colList[1] = { width = 50, label = "#", font = "FIXED", sortable = true }
 	for _, specCol in ipairs(main.curDatFile.spec) do
-		t_insert(self.colList, { width = specCol.width, label = specCol.name, font = function() return IsKeyDown("CTRL") and "FIXED" or "VAR" end })
+		t_insert(self.colList, { 
+			width = specCol.width, 
+			label = specCol.name, 
+			font = function() return IsKeyDown("CTRL") and "FIXED" or "VAR" end,
+			sortable = true -- or false, as needed
+		})
 	end
 	local short = main.curDatFile.rowSize - main.curDatFile.specSize
 	if short > 0 then
-		t_insert(self.colList, { width = short * DrawStringWidth(self.rowHeight, "FIXED", "00 "), font = "FIXED" })
+		t_insert(self.colList, { width = short * DrawStringWidth(self.rowHeight, "FIXED", "00 "), font = "FIXED", sortable = true })
 	end
 end
 
@@ -75,4 +80,159 @@ function RowListClass:GetRowValue(column, index, row)
 			return tostring(data)
 		end
 	end
+end
+
+function RowListClass:Draw(viewPort)
+	local x, y = self:GetPos()
+	local width, height = self:GetSize()
+	local rowHeight = self.rowHeight
+	local list = self.list
+
+	local colOffset = 0
+	for index, column in ipairs(self.colList) do
+		column._offset = colOffset
+		column._width = self:GetColumnProperty(column, "width") or (index == #self.colList and self.scroll and width - 20 or width - colOffset) or 0
+		colOffset = colOffset + column._width
+	end
+
+	local scrollBarV = self.controls.scrollBarV
+	local rowRegion = self:GetRowRegion()
+	scrollBarV:SetContentDimension(#list * rowHeight, rowRegion.height)
+	local scrollOffsetV = scrollBarV.offset
+	local scrollBarH = self.controls.scrollBarH
+	local lastCol = self.colList[#self.colList]
+	scrollBarH:SetContentDimension(lastCol._offset + lastCol._width, rowRegion.width)
+	local scrollOffsetH = scrollBarH.offset
+
+	local cursorX, cursorY = GetCursorPos()
+
+	local label = self:GetProperty("label") 
+	if label then
+		DrawString(x + self.labelPositionOffset[1], y - 20 + self.labelPositionOffset[2], "LEFT", 16, self.font, label)
+	end
+	if self.hasFocus then
+		SetDrawColor(1, 1, 1)
+	else
+		SetDrawColor(0.5, 0.5, 0.5)
+	end
+	DrawImage(nil, x, y, width, height)
+	SetDrawColor(0, 0, 0)
+	DrawImage(nil, x + 1, y + 1, width - 2, height - 2)
+	self:DrawControls(viewPort)
+
+	SetViewport(x + 2, y + 2,  self.scroll and width - 20 or width, height - 4 - (self.scroll and self.scrollH and 16 or 0))
+	local textOffsetY = self.showRowSeparators and 2 or 0
+	local textHeight = rowHeight - textOffsetY * 2
+	local minIndex = math.floor(scrollOffsetV / rowHeight + 1)
+	local maxIndex = math.min(math.floor((scrollOffsetV + height) / rowHeight + 1), #list)
+	for colIndex, column in ipairs(self.colList) do
+		local colFont = self:GetColumnProperty(column, "font") or "VAR"
+		local clipWidth = DrawStringWidth(textHeight, colFont, "...")
+		colOffset = column._offset - scrollOffsetH
+		local colWidth = column._width
+		local relX = cursorX - (x + 2)
+		local relY = cursorY - (y + 2)
+		for index = minIndex, maxIndex do
+			local lineY = rowHeight * (index - 1) - scrollOffsetV + (self.colLabels and 18 or 0)
+			local value = list[index]
+			local text = self:GetRowValue(colIndex, index, value)
+			local textWidth = DrawStringWidth(textHeight, colFont, text)
+			if textWidth > colWidth - 2 then
+				local clipIndex = DrawStringCursorIndex(textHeight, colFont, text, colWidth - clipWidth - 2, 0)
+				text = text:sub(1, clipIndex - 1) .. "..."
+				textWidth = DrawStringWidth(textHeight, colFont, text)
+			end
+			if self.showRowSeparators then
+				if self.hasFocus and value == self.selValue then
+					SetDrawColor(1, 1, 1)
+				else
+					SetDrawColor(0.5, 0.5, 0.5)
+				end
+				DrawImage(nil, colOffset, lineY, not self.scroll and colWidth - 4 or colWidth, rowHeight)
+				if index % 2 == 0 then
+					SetDrawColor(0.05, 0.05, 0.05)
+				else
+					SetDrawColor(0, 0, 0)
+				end
+				DrawImage(nil, colOffset, lineY + 1, not self.scroll and colWidth - 4 or colWidth, rowHeight - 2)
+			elseif value == self.selValue then
+				if self.hasFocus and value == self.selValue then
+					SetDrawColor(1, 1, 1)
+				else
+					SetDrawColor(0.5, 0.5, 0.5)
+				end
+				DrawImage(nil, colOffset, lineY, not self.scroll and colWidth - 4 or colWidth, rowHeight)
+				SetDrawColor(0.15, 0.15, 0.15)
+				DrawImage(nil, colOffset, lineY + 1, not self.scroll and colWidth - 4 or colWidth, rowHeight - 2)
+			end
+			if not self.SetHighlightColor or not self:SetHighlightColor(index, value) then
+				SetDrawColor(1, 1, 1)
+			end
+			DrawString(colOffset, lineY + textOffsetY, "LEFT", textHeight, colFont, text)
+		end
+		if self.colLabels then
+			local mOver = relX >= colOffset and relX <= colOffset + colWidth and relY >= 0 and relY <= 18
+
+			local isSelected = (colIndex - 1) == main.curSpecColIndex
+			local outerColor
+			if mOver then
+				outerColor = {1, 1, 1}
+			elseif isSelected then
+				outerColor = {1, 0.3, 0.2}
+			else
+				outerColor = {0.5, 0.5, 0.5}
+			end
+			local innerColor = isSelected and {0.6, 0.25, 0.2} or (mOver and self:GetColumnProperty(column, "sortable") and {0.33, 0.33, 0.33} or {0.15, 0.15, 0.15})
+
+			SetDrawColor(unpack(outerColor))
+			DrawImage(nil, colOffset, 1, colWidth, 18)
+			SetDrawColor(unpack(innerColor))
+			DrawImage(nil, colOffset + 1, 2, colWidth - 2, 16)
+
+			local label = self:GetColumnProperty(column, "label")
+			if label and #label > 0 then
+				SetDrawColor(1, 1, 1)
+				DrawString(colOffset + colWidth/2, 4, "CENTER_X", 12, "VAR", label)
+			end
+		end
+	end
+	if #self.list == 0 and self.defaultText then
+		SetDrawColor(1, 1, 1)
+		DrawString(2, 2, "LEFT", 14, self.font, self.defaultText)
+	end
+	SetViewport()
+end
+
+function RowListClass:ReSort(colIndex)
+	local asc = true
+	if self.lastSortedCol == colIndex then
+		asc = not self.sortAsc
+	end
+
+	table.sort(self.list, function(a, b)
+		local valA = self:GetRowValue(colIndex, nil, a)
+		local valB = self:GetRowValue(colIndex, nil, b)
+
+		local numA = tonumber(valA)
+		local numB = tonumber(valB)
+
+		if numA and numB then
+			if asc then
+				return numA < numB
+			else
+				return numA > numB
+			end
+		else
+			valA = tostring(valA or "")
+			valB = tostring(valB or "")
+			if asc then
+				return valA < valB
+			else
+				return valA > valB
+			end
+		end
+	end)
+
+	self.lastSortedCol = colIndex
+	self.sortAsc = asc
 end
