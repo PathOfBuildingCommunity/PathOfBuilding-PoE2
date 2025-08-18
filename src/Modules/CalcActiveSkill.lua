PathOfBuildingCommunity/PathOfBuilding-PoE2

commit 4eea0c6fa626198db0c449e7447f5acb58dd7588

Change skills to use new stat set format (#62)
* update skills export

* update gem export files

* fix baseMod directive

* updated gem files

* Add startSets directive to alllow for code against grantedEffect

* minor changes to allow client to load

* Update skill gem tooltips based on statsets

* fix missing quality stats export

* further skill export updates

* Get rid of quality types

* more alt qual removal

* fix support max level

* remove alt qual filter

* alt qual removal

* statset changes

* fix merge dev mistake

* match skill descriptions to in game

* working changes

* revert debug change

* Fix spec

* Fix statset integration

* Add support for gem quality popup

* fix bonestorm statmap

* Fix skillFlags reseting for other environments when creating active skill

* Fix Bonestorm

* Fix export + gem colours

* Export skills

---------

Co-authored-by: LocalIdentity <localidentity2@gmail.com>

src/Modules/CalcActiveSkill.lua
@@ -48,42 +48,49 @@ local function mergeLevelMod(modList, mod, value)
 	end
 end
 
--- Merge skill modifiers with given mod list
-function calcs.mergeSkillInstanceMods(env, modList, skillEffect, extraStats)
+-- Merge skill effect modifiers with given mod list
+-- If a stat set is provided, only merge modifiers from that statset
+function calcs.mergeSkillInstanceMods(env, modList, skillEffect, statSet, extraStats)
 	calcLib.validateGemLevel(skillEffect)
-	local grantedEffect = skillEffect.grantedEffect	
-	local stats = calcLib.buildSkillInstanceStats(skillEffect, grantedEffect)
-	if extraStats and extraStats[1] then
-		for _, stat in pairs(extraStats) do
-			stats[stat.key] = (stats[stat.key] or 0) + stat.value
-		end
+	-- Verify that statSet provided is from skillEffect
+	if statSet and not isValueInArray(skillEffect.grantedEffect.statSets, statSet) then
+		return
 	end
-	for stat, statValue in pairs(stats) do
-		local map = grantedEffect.statMap[stat]
-		if map then
-			-- Some mods need different scalars for different stats, but the same value.  Putting them in a group allows this
-			for _, modOrGroup in ipairs(map) do
-				-- Found a mod, since all mods have names
-				if modOrGroup.name then
-					mergeLevelMod(modList, modOrGroup, map.value or statValue * (map.mult or 1) / (map.div or 1) + (map.base or 0))
-				else
-					for _, mod in ipairs(modOrGroup) do
-						mergeLevelMod(modList, mod, modOrGroup.value or statValue * (modOrGroup.mult or 1) / (modOrGroup.div or 1) + (modOrGroup.base or 0))
+	local grantedEffect = skillEffect.grantedEffect	
+	for _, statSet in ipairs(statSet and {statSet} or grantedEffect.statSets) do 
+		local stats = calcLib.buildSkillInstanceStats(skillEffect, grantedEffect, statSet)
+		if extraStats and extraStats[1] then
+			for _, stat in pairs(extraStats) do
+				stats[stat.key] = (stats[stat.key] or 0) + stat.value
+			end
+		end
+		for stat, statValue in pairs(stats) do
+			local map = statSet.statMap[stat]
+			if map then
+				-- Some mods need different scalars for different stats, but the same value.  Putting them in a group allows this
+				for _, modOrGroup in ipairs(map) do
+					-- Found a mod, since all mods have names
+					if modOrGroup.name then
+						mergeLevelMod(modList, modOrGroup, map.value or statValue * (map.mult or 1) / (map.div or 1) + (map.base or 0))
+					else
+						for _, mod in ipairs(modOrGroup) do
+							mergeLevelMod(modList, mod, modOrGroup.value or statValue * (modOrGroup.mult or 1) / (modOrGroup.div or 1) + (modOrGroup.base or 0))
+						end
 					end
 				end
 			end
 		end
+		modList:AddList(statSet.baseMods)
 	end
-	modList:AddList(grantedEffect.baseMods)
 end
 
 -- Create an active skill using the given active gem and list of support gems
 -- It will determine the base flag set, and check which of the support gems can support this skill
-function calcs.createActiveSkill(activeEffect, supportList, actor, socketGroup, summonSkill)
+function calcs.createActiveSkill(activeEffect, supportList, env, socketGroup, summonSkill)
 	local activeSkill = {
 		activeEffect = activeEffect,
 		supportList = supportList,
-		actor = actor,
+		actor = env.player,
 		summonSkill = summonSkill,
 		socketGroup = socketGroup,
 		skillData = { },
@@ -99,8 +106,22 @@ function calcs.createActiveSkill(activeEffect, supportList, actor, socketGroup, 
 	end
 
 	-- Initialise skill flag set ('attack', 'projectile', etc)
-	local skillFlags = copyTable(activeGrantedEffect.baseFlags)
-	activeSkill.skillFlags = skillFlags
+	local skillFlags
+	if env.mode == "CALCS" then 
+		if activeEffect.srcInstance.statSetCalcs.statSet then
+			skillFlags = copyTable(activeEffect.srcInstance.statSetCalcs.statSet.baseFlags)
+		else
+			skillFlags = copyTable(activeEffect.grantedEffect.statSets[1].baseFlags)
+		end
+		activeEffect.srcInstance.statSetCalcs.skillFlags = skillFlags
+	else 
+		if activeEffect.srcInstance.statSetMain.statSet then
+			skillFlags = copyTable(activeEffect.srcInstance.statSetMain.statSet.baseFlags)
+		else
+			skillFlags = copyTable(activeEffect.grantedEffect.statSets[1].baseFlags)
+		end
+		activeEffect.srcInstance.statSetMain.skillFlags = skillFlags
+	end
 	skillFlags.hit = skillFlags.hit or activeSkill.skillTypes[SkillType.Attack] or activeSkill.skillTypes[SkillType.Damage] or activeSkill.skillTypes[SkillType.Projectile]
 
 	-- Process support skills
@@ -145,7 +166,8 @@ function calcs.createActiveSkill(activeEffect, supportList, actor, socketGroup, 
 			if supportEffect.grantedEffect.addFlags and not summonSkill then
 				-- Support skill adds flags to supported skills (eg. Remote Mine adds 'mine')
 				for k in pairs(supportEffect.grantedEffect.addFlags) do
-					skillFlags[k] = true
+					mainSkillFlags[k] = true
+					calcsSkillFlags[k] = true
 				end
 			end
 		end
@@ -160,11 +182,10 @@ function calcs.copyActiveSkill(env, mode, skill)
 		grantedEffect = skill.activeEffect.grantedEffect,
 		level = skill.activeEffect.srcInstance.level,
 		quality = skill.activeEffect.srcInstance.quality,
-		qualityId = skill.activeEffect.srcInstance.qualityId,
 		srcInstance = skill.activeEffect.srcInstance,
 		gemData = skill.activeEffect.srcInstance.gemData,
 	}
-	local newSkill = calcs.createActiveSkill(activeEffect, skill.supportList, skill.actor, skill.socketGroup, skill.summonSkill)
+	local newSkill = calcs.createActiveSkill(activeEffect, skill.supportList, env, skill.socketGroup, skill.summonSkill)
 	local newEnv, _, _, _ = calcs.initEnv(env.build, mode, env.override)
 	calcs.buildActiveSkillModList(newEnv, newSkill)
 	newSkill.skillModList = new("ModList", newSkill.baseSkillModList)
@@ -215,9 +236,16 @@ end
 -- Build list of modifiers for given active skill
 function calcs.buildActiveSkillModList(env, activeSkill)
 	local skillTypes = activeSkill.skillTypes
-	local skillFlags = activeSkill.skillFlags
 	local activeEffect = activeSkill.activeEffect
 	local activeGrantedEffect = activeEffect.grantedEffect
+	local activeStatSet, skillFlags
+	if env.mode == "CALCS" then
+		activeStatSet = activeEffect.srcInstance.statSetCalcs.statSet
+		skillFlags = activeEffect.srcInstance.statSetCalcs.skillFlags
+	else
+		activeStatSet = activeEffect.srcInstance.statSetMain.statSet
+		skillFlags = activeEffect.srcInstance.statSetMain.skillFlags
+	end
 	local effectiveRange = 0
 
 	-- Set mode flags
@@ -518,32 +546,34 @@ function calcs.buildActiveSkillModList(env, activeSkill)
 
 	-- Add active gem modifiers
 	activeEffect.actorLevel = activeSkill.actor.minionData and activeSkill.actor.level
-	calcs.mergeSkillInstanceMods(env, skillModList, activeEffect, skillModList:List(activeSkill.skillCfg, "ExtraSkillStat"))
+	calcs.mergeSkillInstanceMods(env, skillModList, activeEffect, activeStatSet, skillModList:List(activeSkill.skillCfg, "ExtraSkillStat"))
 	activeEffect.grantedEffectLevel = activeGrantedEffect.levels[activeEffect.level]
 
 	-- Add extra modifiers from granted effect level
-	local level = activeEffect.grantedEffectLevel
-	activeSkill.skillData.CritChance = level.critChance
-	if level.damageMultiplier then
-		skillModList:NewMod("Damage", "MORE", level.damageMultiplier, activeEffect.grantedEffect.modSource, ModFlag.Attack)
+	local effectLevel = activeEffect.grantedEffectLevel
+	local setLevel = activeStatSet.levels[activeEffect.level] 
+	-- THIS PROBABLY NEEDS TO BE FIXED TO INHERIT CRIT CHANCE SOMEHOW
+	activeSkill.skillData.CritChance = setLevel and setLevel.critChance or 0 
+	if effectLevel.damageMultiplier then
+		skillModList:NewMod("Damage", "MORE", effectLevel.damageMultiplier, activeEffect.grantedEffect.modSource, ModFlag.Attack)
 	end
-	if level.attackTime then
-		activeSkill.skillData.attackTime = level.attackTime
+	if effectLevel.attackTime then
+		activeSkill.skillData.attackTime = effectLevel.attackTime
 	end
-	if level.attackSpeedMultiplier then
-		skillModList:NewMod("Speed", "MORE", level.attackSpeedMultiplier, activeEffect.grantedEffect.modSource, ModFlag.Attack)
+	if effectLevel.attackSpeedMultiplier then
+		skillModList:NewMod("Speed", "MORE", effectLevel.attackSpeedMultiplier, activeEffect.grantedEffect.modSource, ModFlag.Attack)
 	end
-	if level.cooldown then
-		activeSkill.skillData.cooldown = level.cooldown
+	if effectLevel.cooldown then
+		activeSkill.skillData.cooldown = effectLevel.cooldown
 	end
-	if level.storedUses then
-		activeSkill.skillData.storedUses = level.storedUses
+	if effectLevel.storedUses then
+		activeSkill.skillData.storedUses = effectLevel.storedUses
 	end
-	if level.soulPreventionDuration then
-		activeSkill.skillData.soulPreventionDuration = level.soulPreventionDuration
+	if effectLevel.soulPreventionDuration then
+		activeSkill.skillData.soulPreventionDuration = effectLevel.soulPreventionDuration
 	end
-	if level.PvPDamageMultiplier then
-		skillModList:NewMod("PvpDamageMultiplier", "MORE", level.PvPDamageMultiplier, activeEffect.grantedEffect.modSource)
+	if effectLevel.PvPDamageMultiplier then
+		skillModList:NewMod("PvpDamageMultiplier", "MORE", effectLevel.PvPDamageMultiplier, activeEffect.grantedEffect.modSource)
 	end
 	
 	-- Add extra modifiers from other sources

src/Classes/CalcsTab.lua
@@ -58,6 +58,16 @@ local CalcsTabClass = newClass("CalcsTab", "UndoHandler", "ControlHost", "Contro
 				self.build.buildFlag = true
 			end)
 		}, },
+		{ label = "Stat Set", { controlName = "statSet", 
+			control = new("DropDownControl", nil, {0, 0, 300, 16}, nil, function(index, value)
+				local mainSocketGroup = self.build.skillsTab.socketGroupList[self.input.skill_number]
+				local srcInstance = mainSocketGroup.displaySkillListCalcs[mainSocketGroup.mainActiveSkillCalcs].activeEffect.srcInstance
+				srcInstance.statSetCalcs = srcInstance.statSetCalcs or { }
+				srcInstance.statSetCalcs.statSet = value.statSet
+				self:AddUndoState()
+				self.build.buildFlag = true
+			end)
+		}, },
 		{ label = "Skill Part", playerFlag = "multiPart", { controlName = "mainSkillPart", 
 			control = new("DropDownControl", nil, {0, 0, 250, 16}, nil, function(index, value)
 				local mainSocketGroup = self.build.skillsTab.socketGroupList[self.input.skill_number]
@@ -374,7 +384,7 @@ end
 
 function CalcsTabClass:CheckFlag(obj)
 	local actor = self.input.showMinion and self.calcsEnv.minion or self.calcsEnv.player
-	local skillFlags = actor.mainSkill.skillFlags
+	local skillFlags = actor.mainSkill.activeEffect.srcInstance.statSetCalcs.skillFlags
 	if obj.flag and not skillFlags[obj.flag] then
 		return
 	end
@@ -385,7 +395,7 @@ function CalcsTabClass:CheckFlag(obj)
 			end
 		end
 	end
-	if obj.playerFlag and not self.calcsEnv.player.mainSkill.skillFlags[obj.playerFlag] then
+	if obj.playerFlag and not self.calcsEnv.player.mainSkill.activeEffect.srcInstance.statSetCalcs.skillFlags[obj.playerFlag] then
 		return
 	end
 	if obj.notFlag and skillFlags[obj.notFlag] then

src/Classes/ConfigTab.lua
@@ -448,7 +448,8 @@ local ConfigTabClass = newClass("ConfigTab", "UndoHandler", "ControlHost", "Cont
 			if varData.ifFlag then
 				t_insert(shownFuncs, listOrSingleIfOption(varData.ifFlag, function(ifOption)
 					local skillModList = self.build.calcsTab.mainEnv.player.mainSkill.skillModList
-					local skillFlags = self.build.calcsTab.mainEnv.player.mainSkill.skillFlags
+					-- only checking flags of skill in main env. rework may be required
+					local skillFlags = self.build.calcsTab.mainEnv.player.mainSkill.activeEffect.srcInstance.statSetMain.skillFlags
 					-- Check both the skill mods for flags and flags that are set via calcPerform
 					return skillFlags[ifOption] or skillModList:Flag(nil, ifOption)
 				end))
@@ -497,7 +498,8 @@ local ConfigTabClass = newClass("ConfigTab", "UndoHandler", "ControlHost", "Cont
 			if varData.ifSkillFlag then
 				t_insert(shownFuncs, listOrSingleIfOption(varData.ifSkillFlag, function(ifOption)
 					for _, activeSkill in ipairs(self.build.calcsTab.mainEnv.player.activeSkillList) do
-						if activeSkill.skillFlags[ifOption] then
+						-- only checking flags of skill in main env. rework may be required
+						if activeSkill.activeEffect.srcInstance.statSetMain.skillFlags[ifOption] then
 							return true
 						end
 					end

src/Classes/GemSelectControl.lua
@@ -12,12 +12,6 @@ local m_max = math.max
 local m_floor = math.floor
 
 local toolTipText = "Prefix tag searches with a colon and exclude tags with a dash. e.g. :fire:lightning:-cold:area"
-local altQualMap = {
-	["Default"] = "",
-	["Alternate1"] = "Anomalous ",
-	["Alternate2"] = "Divergent ",
-	["Alternate3"] = "Phantasmal ",
-}
 
 local GemSelectClass = newClass("GemSelectControl", "EditControl", function(self, anchor, rect, skillsTab, index, changeFunc, forceTooltip)
 	self.EditControl(anchor, rect, nil, nil, "^ %a':-")
@@ -58,7 +52,7 @@ local GemSelectClass = newClass("GemSelectControl", "EditControl", function(self
 	}
 end)
 
-function GemSelectClass:CalcOutputWithThisGem(calcFunc, gemData, qualityId, useFullDPS)
+function GemSelectClass:CalcOutputWithThisGem(calcFunc, gemData, useFullDPS)
 	local gemList = self.skillsTab.displayGroup.gemList
 	local oldGem
 	if gemList[self.index] then
@@ -66,7 +60,6 @@ function GemSelectClass:CalcOutputWithThisGem(calcFunc, gemData, qualityId, useF
 	else
 		gemList[self.index] = {
 			level = gemData.naturalMaxLevel,
-			qualityId = qualityId,
 			quality = self.skillsTab.defaultGemQuality or 0,
 			count = 1,
 			enabled = true,
@@ -83,9 +76,6 @@ function GemSelectClass:CalcOutputWithThisGem(calcFunc, gemData, qualityId, useF
 	gemInstance.level = self.skillsTab:ProcessGemLevel(gemData)
 	gemInstance.gemData = gemData
 	gemInstance.displayEffect = nil
-	if gemInstance.qualityId == nil or gemInstance.qualityId == "" then
-		gemInstance.qualityId = "Default"
-	end
 	-- Add hovered gem to tooltip
 	self:AddGemTooltip(gemInstance)
 	-- Calculate the impact of using this gem
@@ -114,17 +104,7 @@ function GemSelectClass:PopulateGemList()
 		if (self.sortGemsBy and gemData.tags[self.sortGemsBy] == true or not self.sortGemsBy) then
 			local levelRequirement = (gemData.grantedEffect.levels and gemData.grantedEffect.levels[1] and gemData.grantedEffect.levels[1].levelRequirement) or 1
 			if characterLevel >= levelRequirement or not matchLevel then
-				if (showAwakened or showAll) and gemData.grantedEffect.plusVersionOf then
-					self.gems["Default:" .. gemId] = gemData
-				elseif showNormal or showAll then
-					if self.skillsTab.showAltQualityGems and (self.skillsTab.defaultGemQuality or 0) > 0 then
-						for _, altQual in ipairs(self.skillsTab:getGemAltQualityList(gemData)) do
-							self.gems[altQual.type .. ":" .. gemId] = gemData
-						end
-					else
-						self.gems["Default:" .. gemId] = gemData
-					end
-				end
+				self.gems["Default:" .. gemId] = gemData
 			end
 		end
 	end
@@ -140,7 +120,6 @@ function GemSelectClass:FilterSupport(gemId, gemData)
 		or showSupportTypes == "ALL"
 		or (showSupportTypes == "NORMAL" and not gemData.grantedEffect.plusVersionOf)
 		or (showSupportTypes == "AWAKENED" and gemData.grantedEffect.plusVersionOf))
-		and (self.skillsTab.showAltQualityGems or (not self.skillsTab.showAltQualityGems and self:GetQualityType(gemId) == "Default"))
 end
 
 function GemSelectClass:BuildList(buf)
@@ -170,7 +149,7 @@ function GemSelectClass:BuildList(buf)
 		for i, pattern in ipairs(patternList) do
 			local matchList = { }
 			for gemId, gemData in pairs(self.gems) do
-				if self:FilterSupport(gemId, gemData) and not added[gemId] and ((" "..gemData.name:lower()):match(pattern) or altQualMap[self:GetQualityType(gemId)]:lower():match(pattern)) then
+				if self:FilterSupport(gemId, gemData) and not added[gemId] and ((" "..gemData.name:lower()):match(pattern)) then
 					addThisGem = true
 					if #tagsList > 0 then
 						for _, tag in ipairs(tagsList) do
@@ -250,11 +229,11 @@ function GemSelectClass:UpdateSortCache()
 		and sortCache.outputRevision == self.skillsTab.build.outputRevision and sortCache.defaultLevel == self.skillsTab.defaultGemLevel
 		and (sortCache.characterLevel == self.skillsTab.build.characterLevel or self.skillsTab.defaultGemLevel ~= "characterLevel")
 		and sortCache.defaultQuality == self.skillsTab.defaultGemQuality and sortCache.sortType == self.skillsTab.sortGemsByDPSField
-		and sortCache.considerAlternates == self.skillsTab.showAltQualityGems and sortCache.considerGemType == self.skillsTab.showSupportGemTypes then
+		and sortCache.considerGemType == self.skillsTab.showSupportGemTypes then
 		return
 	end
 
-	if not sameSortBy or not sortCache or (sortCache.considerAlternates ~= self.skillsTab.showAltQualityGems or sortCache.considerGemType ~= self.skillsTab.showSupportGemTypes
+	if not sameSortBy or not sortCache or (sortCache.considerGemType ~= self.skillsTab.showSupportGemTypes
 		or sortCache.defaultQuality ~= self.skillsTab.defaultGemQuality
 		or sortCache.defaultLevel ~= self.skillsTab.defaultGemLevel
 		or (sortCache.characterLevel ~= self.skillsTab.build.characterLevel and self.skillsTab.defaultGemLevel == "characterLevel")) then
@@ -265,7 +244,6 @@ function GemSelectClass:UpdateSortCache()
 	-- Initialize a new sort cache
 	sortCache = {
 		considerGemType = self.skillsTab.showSupportGemTypes,
-		considerAlternates = self.skillsTab.showAltQualityGems,
 		socketGroup = self.skillsTab.displayGroup,
 		gemInstance = self.skillsTab.displayGroup.gemList[self.index],
 		outputRevision = self.skillsTab.build.outputRevision,
@@ -334,7 +312,7 @@ function GemSelectClass:UpdateSortCache()
 		sortCache.dps[gemId] = baseDPS
 		-- Ignore gems that don't support the active skill
 		if sortCache.canSupport[gemId] or gemData.grantedEffect.hasGlobalEffect then
-			local output = self:CalcOutputWithThisGem(calcFunc, gemData, self:GetQualityType(gemId), useFullDPS)
+			local output = self:CalcOutputWithThisGem(calcFunc, gemData, useFullDPS)
 			-- Check for nil because some fields may not be populated, default to 0
 			sortCache.dps[gemId] = (dpsField == "FullDPS" and output[dpsField] ~= nil and output[dpsField]) or (output.Minion and output.Minion.CombinedDPS) or (output[dpsField] ~= nil and output[dpsField]) or 0
 		end
@@ -457,9 +435,6 @@ function GemSelectClass:Draw(viewPort, noTooltip)
 				end
 			end
 			local gemText = gemData and gemData.name or "<No matches>"
-			if gemId and gemId ~= "" then
-				gemText = altQualMap[self:GetQualityType(gemId)] .. gemText
-			end
 			DrawString(0, y, "LEFT", height - 4, "VAR", gemText)
 			if gemData then
 				if gemData.grantedEffect.support and self.sortCache.canSupport[gemId] then
@@ -477,7 +452,7 @@ function GemSelectClass:Draw(viewPort, noTooltip)
 			local calcFunc, calcBase = self.skillsTab.build.calcsTab:GetMiscCalculator(self.build)
 			if calcFunc then
 				self.tooltip:Clear()
-				local output, gemInstance = self:CalcOutputWithThisGem(calcFunc, self.gems[self.list[self.hoverSel]], self:GetQualityType(self.list[self.hoverSel]), self.skillsTab.sortGemsByDPSField == "FullDPS")
+				local output = self:CalcOutputWithThisGem(calcFunc, self.gems[self.list[self.hoverSel]], self.skillsTab.sortGemsByDPSField == "FullDPS")
 				self.tooltip:AddSeparator(10)
 				self.skillsTab.build:AddStatComparesToTooltip(self.tooltip, calcBase, output, "^7Selecting this gem will give you:")
 				self.tooltip:Draw(x, y + height + 2 + (self.hoverSel - 1) * (height - 4) - scrollBar.offset, width, height - 4, viewPort)
@@ -506,10 +481,6 @@ function GemSelectClass:Draw(viewPort, noTooltip)
 			local cursorX, cursorY = GetCursorPos()
 			self.tooltip:Clear()
 			if gemInstance and gemInstance.gemData then
-				-- Check valid qualityId, set to 'Default' if missing
-				if gemInstance.qualityId == nil or gemInstance.qualityId == "" then
-					gemInstance.qualityId = "Default"
-				end
 				self:AddGemTooltip(gemInstance)
 			else
 				self.tooltip:AddLine(16, toolTipText)
@@ -564,25 +535,29 @@ function GemSelectClass:AddGemTooltip(gemInstance)
 	local grantedEffect = gemInstance.gemData.grantedEffect
 	local additionalEffects = gemInstance.gemData.additionalGrantedEffects
 
-	self.tooltip:AddLine(20, colorCodes.GEM .. altQualMap[gemInstance.qualityId]..grantedEffect.name)
+	self.tooltip:AddLine(20, colorCodes.GEM .. grantedEffect.name)
 	self.tooltip:AddSeparator(10)
 	self.tooltip:AddLine(16, "^x7F7F7F" .. gemInstance.gemData.tagString)
 	-- Will need rework if a gem can have 2+ additional supports
-	self:AddCommonGemInfo(gemInstance, grantedEffect, true, additionalEffects[1] and additionalEffects[1].support and additionalEffects[1])
+	self:AddGrantedEffectInfo(gemInstance, grantedEffect, true)
+	for _, statSet in ipairs(grantedEffect.statSets) do
+		self:AddStatSetInfo(gemInstance, grantedEffect, statSet)
+	end
 
-	if additionalEffects then
-		for _, additional in ipairs(additionalEffects) do
-			if not additional.support then
-				self.tooltip:AddSeparator(10)
-				self.tooltip:AddLine(20, colorCodes.GEM .. additional.name)
-				self.tooltip:AddSeparator(10)
-				self:AddCommonGemInfo(gemInstance, additional)
+	for _, additional in ipairs(additionalEffects or {}) do
+		if not additional.support then
+			self.tooltip:AddSeparator(10)
+			self.tooltip:AddLine(20, colorCodes.GEM .. additional.name)
+			self.tooltip:AddSeparator(10)
+			self:AddGrantedEffectInfo(gemInstance, additional)
+			for _, statSet in ipairs(additional.statSets) do
+				self:AddStatSetInfo(gemInstance, grantedEffect, statSet)
 			end
 		end
 	end
 end
 
-function GemSelectClass:AddCommonGemInfo(gemInstance, grantedEffect, addReq, mergeStatsFrom)
+function GemSelectClass:AddGrantedEffectInfo(gemInstance, grantedEffect, addReq)
 	local displayInstance = gemInstance.displayEffect or gemInstance
 	local grantedEffectLevel = grantedEffect.levels[displayInstance.level] or { }
 	if addReq then
@@ -661,12 +636,6 @@ function GemSelectClass:AddCommonGemInfo(gemInstance, grantedEffect, addReq, mer
 				self.tooltip:AddLine(16, "^x7F7F7FCast Time: ^7Instant")
 			end
 		end
-		if grantedEffectLevel.critChance then
-			self.tooltip:AddLine(16, string.format("^x7F7F7FCritical Hit Chance: ^7%.2f%%", grantedEffectLevel.critChance))
-		end
-		if grantedEffectLevel.damageEffectiveness then
-			self.tooltip:AddLine(16, string.format("^x7F7F7FEffectiveness of Added Damage: ^7%d%%", grantedEffectLevel.damageEffectiveness * 100))
-		end
 	end
 	if addReq and displayInstance.quality > 0 then
 		self.tooltip:AddLine(16, string.format("^x7F7F7FQuality: "..colorCodes.MAGIC.."+%d%%^7%s",
@@ -688,17 +657,30 @@ function GemSelectClass:AddCommonGemInfo(gemInstance, grantedEffect, addReq, mer
 			self.tooltip:AddLine(16, colorCodes.GEM..line)
 		end
 	end
-	if self.skillsTab.build.data.describeStats then
+end
+function GemSelectClass:AddStatSetInfo(gemInstance, grantedEffect, statSet)
+	local displayInstance = gemInstance.displayEffect or gemInstance
+	local statSetLevel = statSet.levels[displayInstance.level] or { }
+	self.tooltip:AddSeparator(10)
+	self.tooltip:AddLine(20, colorCodes.GEM .. statSet.label)
+	self.tooltip:AddSeparator(10)
+	if statSetLevel.critChance then
+		self.tooltip:AddLine(16, string.format("^x7F7F7FCritical Hit Chance: ^7%.2f%%", statSetLevel.critChance))
+	end
+	if statSetLevel.baseMultiplier then
+		self.tooltip:AddLine(16, string.format("^x7F7F7FAttack Damage: ^7%d%%", statSetLevel.baseMultiplier * 100))
+	end
+	if self.skillsTab and self.skillsTab.build.data.describeStats then
 		self.tooltip:AddSeparator(10)
-		local stats = calcLib.buildSkillInstanceStats(displayInstance, grantedEffect)
-		if mergeStatsFrom then
-			for stat, val in pairs(calcLib.buildSkillInstanceStats(displayInstance, mergeStatsFrom)) do
-				stats[stat] = (stats[stat] or 0) + val
-			end
-		end
-		local descriptions, lineMap = self.skillsTab.build.data.describeStats(stats, grantedEffect.statDescriptionScope)
+		local stats = calcLib.buildSkillInstanceStats(displayInstance, grantedEffect, statSet)
+		--if mergeStatsFrom then
+		--	for stat, val in pairs(calcLib.buildSkillInstanceStats(displayInstance, mergeStatsFrom)) do
+		--		stats[stat] = (stats[stat] or 0) + val
+		--	end
+		--end
+		local descriptions, lineMap = self.skillsTab.build.data.describeStats(stats, statSet.statDescriptionScope)
 		for _, line in ipairs(descriptions) do
-			local source = grantedEffect.statMap[lineMap[line]] or self.skillsTab.build.data.skillStatMap[lineMap[line]]
+			local source = statSet.statMap[lineMap[line]] or self.skillsTab.build.data.skillStatMap[lineMap[line]]
 			if source then
 				if launch.devModeAlt then
 					local devText = lineMap[line]
@@ -720,7 +702,6 @@ function GemSelectClass:AddCommonGemInfo(gemInstance, grantedEffect, addReq, mer
 		end
 	end
 end
-
 function GemSelectClass:OnFocusGained()
 	self.EditControl:OnFocusGained()
 	self.dropped = true

src/Classes/ModStore.lua
@@ -238,7 +238,8 @@ function ModStoreClass:GetStat(stat, cfg)
 		local totalMana = self.actor.output["Mana"]
 		if totalMana == 0 then return 0 else
 			for _, activeSkill in ipairs(self.actor.activeSkillList) do
-				if (activeSkill.skillTypes[SkillType.Aura] and not activeSkill.skillFlags.disable and activeSkill.buffList and activeSkill.buffList[1] and activeSkill.buffList[1].name == cfg.skillName) then
+				-- currently only checks main statset for skill flags. rework if required
+				if (activeSkill.skillTypes[SkillType.Aura] and not activeSkill.activeEffect.srcInstance.statSetMain.skillFlags.disable and activeSkill.buffList and activeSkill.buffList[1] and activeSkill.buffList[1].name == cfg.skillName) then
 					local manaBase = activeSkill.skillData["ManaReservedBase"] or 0
 					reservedPercentMana = manaBase / totalMana * 100
 					break

commit 2894493848931210b773c37dd257fec0dc93a06a

Fix `addFlags` parsing
`addFlags` functionality was accidentally broken in #62 because
`mainSkillFlags` and `calcSkillFlags` variables were introduced but
seemingly later made redundant to use the original `skillFlags`.

However the `addFlags` section still checked the old variables which were
now `nil`. This wasn't a problem until now, because the PoE2 skills had
not used `addFlags` yet.

src/Modules/CalcActiveSkill.lua
@@ -165,8 +165,7 @@ function calcs.createActiveSkill(activeEffect, supportList, env, actor, socketGr
 			if supportEffect.grantedEffect.addFlags and not summonSkill then
 				-- Support skill adds flags to supported skills (eg. Remote Mine adds 'mine')
 				for k in pairs(supportEffect.grantedEffect.addFlags) do
-					mainSkillFlags[k] = true
-					calcsSkillFlags[k] = true
+					skillFlags[k] = true
 				end
 			end
 		end