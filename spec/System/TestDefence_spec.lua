1b1ceb9a20551746902af59a2a00396998b3f163
LocalIdentity <localidentity2@gmail.com>
2025-09-10 00:50:02 +1000

    Fix tests attempt #2

--- spec/System/TestDefence_spec.lua
+++ spec/System/TestDefence_spec.lua
@@ -10,9 +10,11 @@ describe("TestDefence", function()
 	local function pob1and2Compat()
 		build.configTab.input.customMods = build.configTab.input.customMods.."\n\z
 		5% reduced maximum life\n\z
+		5% reduced maximum mana\n\z
 		-2 to life\n\z
 		-10% to elemental resistances\n\z
 		-60% to chaos resistance\n\z
+		+2 to mana\n\z
 		"
 		build.configTab:BuildModList()
 		runCallback("OnFrame")
