55f897ed8ae59acc1f78f33f90a8163cf35907c0
Musholic <musholic.tbg@gmail.com>
2025-09-15 19:08:55 +0200

    Support for multiple contributors and/or PRs in changelog

--- .github/tweak_changelogs.sh
+++ .github/tweak_changelogs.sh
@@ -13,9 +13,16 @@ sed -i '1h;1d;$!H;$!d;G' temp_change.md
 sed -i -re 's/\*\*Full Changelog\*\*: (.*)/\[Full Changelog\]\(\1\)\n/' temp_change.md
 # Delete everything from "## New Contributors" line to the end of file
 sed -i '/## New Contributors/,$d' temp_change.md
-# Convert GitHub changelog entries from "* description by @username in pull/URL/number"
-# to "- description [#number](pull/URL/number) ([username](https://github.com/username))" format
-sed -i -re 's/^\*(.*)\sby\s@(.*)\sin\s(.*\/pull\/)(.*)/-\1 [\\#\4](\3\4) ([\2](https:\/\/github.com\/\2))/' temp_change.md;
+# Convert GitHub changelog entries to markdown format
+# "* description by (@username1, @username2) in #1310, #1311" → "- description #1310, #1311 (@username1, @username2)"
+sed -i -re 's/^\*\s(.*)\sby\s\(?(@[^)]*[^) ])\)?\s+in\s+(.*)/- \1 \3 (\2)/' temp_change.md
+# Convert @usernames to github links
+# "(@username1, @username2)" → "([username1](https://github.com/username1), [username2](https://github.com/username2))"
+sed -i -re 's/@([a-zA-Z0-9_-]+)/[\1](https:\/\/github.com\/\1)/g' temp_change.md
+# Convert full PR URLs to linked format  
+# "https://github.com/repo/pull/1310" → "[\#1310](https://github.com/repo/pull/1310)"
+sed -i -re 's/(https:\/\/[^) ]*\/pull\/([0-9]+))/[\\#\2](\1)/g' temp_change.md
+
 # Username substitutions for preferred display names
 sed -i 's/\[Quotae/\[Quote_a/' temp_change.md
 sed -i 's/\[learn2draw/\[Lexy/' temp_change.md
@@ -29,7 +36,10 @@ cat CHANGELOG.md | sed '1d' >> temp_change.md
 # Create new CHANGELOG.md with header containing version and date, followed by processed changes
 printf "# Changelog\n\n## [$RELEASE_VERSION](https://github.com/PathOfBuildingCommunity/PathOfBuilding-PoE2/tree/$RELEASE_VERSION) ($(date +'%Y/%m/%d'))\n\n" | cat - temp_change.md > CHANGELOG.md
 # Convert changelog entries from markdown link format to simplified "* description (username)" format
-sed -i -re 's/^- (.*) \[.*\) \(\[(.*)\]\(.*/* \1 (\2)/' changelog_temp.txt
+# First remove all PR links
+sed -i -re 's/( \()?\[\\#[0-9]+\]\([^)]*\),? ?\)?//g' changelog_temp.txt
+# Remove markdown link formatting from usernames in parentheses
+sed -i -re 's/\[([^]]*)\]\(https:\/\/github\.com\/[^)]*\)/\1/g' changelog_temp.txt
 # Create new changelog format: add version header, remove lines 2-3, format section headers, remove ## headers with following line, prepend to existing changelog
 echo "VERSION[${RELEASE_VERSION#v}][$(date +'%Y/%m/%d')]" | cat - changelog_temp.txt | sed '2,3d' | sed -re 's/^### (.*)/\n--- \1 ---/' | sed -e '/^##.*/,+1 d' | cat - changelog.txt > changelog_new.txt
 mv changelog_new.txt changelog.txt
