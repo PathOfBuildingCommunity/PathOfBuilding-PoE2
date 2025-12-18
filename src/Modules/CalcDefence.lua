c0eef2cd512ef5c2ac6eaa05cf1995e55dc7c62e
LocalIdentity <localidentity2@gmail.com>
2025-12-18 12:24:33 +1100

    0.4 Patch note changes
    Min armour damage reduction is 1 now
    Surrounded has a minimum of 1 enemy
    Tailwind buff stat changes
    Basic attack attack speed change

--- src/Modules/CalcDefence.lua
+++ src/Modules/CalcDefence.lua
@@ -59,7 +59,7 @@ function calcs.armourReductionF(armour, raw)
 		armour = -armour -- revert value to positive for calculation
 		return -((armour / (armour + raw * data.misc.ArmourRatio) * 100))
 	end
-	return (armour / (armour + raw * data.misc.ArmourRatio) * 100)
+	return m_max((armour / (armour + raw * data.misc.ArmourRatio) * 100), 1)
 end
 
 -- Calculate damage reduction from armour, int
