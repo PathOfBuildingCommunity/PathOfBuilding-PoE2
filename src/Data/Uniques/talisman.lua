6c1475addce1b0c1b42e6dbf877cac93566f1953
Blitz54 <football_and_beer@msn.com>
2026-01-11 03:27:30 -0600

    Hysseg's Claw

--- src/Data/Uniques/talisman.lua
+++ src/Data/Uniques/talisman.lua
@@ -10,5 +10,17 @@ Adds (16-20) to (23-27) Physical Damage
 (10-15)% increased Skill Effect Duration
 Enemies in your Presence are Hindered
 Gain 1 Druidic Prowess for every 20 total Rage spent
+]],[[
+Hysseg's Claw
+Familial Talisman
+Implicits: 2
+Grants Skill: Level (1-20) Cackling Companions
+Minions deal (30-50)% increased Damage
+(70-100)% increased Physical Damage
+5% increased Movement Speed
++(8-15) to Strength
++(8-15) to Intelligence
+(5-8)% increased Damage per Minion
 ]],
+
 }
--- src/Export/Uniques/talisman.lua
+++ src/Export/Uniques/talisman.lua
@@ -10,5 +10,17 @@ UniqueIntelligence45
 UniqueSkillEffectDuration2
 UniqueHinderEnemiesInPresence1
 UniqueGainDruidicProwessOnSpendingXRage1
+]],[[
+Hysseg's Claw
+Familial Talisman
+Implicits: 2
+Grants Skill: Level (1-20) Cackling Companions
+TalismanImplicitMinionDamage1
+UniqueLocalIncreasedPhysicalDamagePercent17
+UniqueMovementVelocity26
+UniqueStrength40
+UniqueIntelligence45
+UniqueDamagePerMinion1
 ]],
+
 }
