6556bbfffc96377a5c71d6b320900a71081ca1ca
Blitz54 <football_and_beer@msn.com>
2026-01-11 03:21:55 -0600

    Amor Mandragora

--- src/Data/Uniques/talisman.lua
+++ src/Data/Uniques/talisman.lua
@@ -1,3 +1,14 @@
 -- Item data (c) Grinding Gear Games
 
-return {}
+return {
+-- Weapon: Talisman
+[[
+Amor Mandragora
+Changeling Talisman
+Adds (16-20) to (23-27) Physical Damage
++(8-15) to Intelligence
+(10-15)% increased Skill Effect Duration
+Enemies in your Presence are Hindered
+Gain 1 Druidic Prowess for every 20 total Rage spent
+]],
+}
---
+++ src/Export/Uniques/talisman.lua
@@ -0,0 +1,14 @@
+-- Item data (c) Grinding Gear Games
+
+return {
+-- Weapon: Talisman
+[[
+Amor Mandragora
+Changeling Talisman
+UniqueLocalAddedPhysicalDamage27
+UniqueIntelligence45
+UniqueSkillEffectDuration2
+UniqueHinderEnemiesInPresence1
+UniqueGainDruidicProwessOnSpendingXRage1
+]],
+}
