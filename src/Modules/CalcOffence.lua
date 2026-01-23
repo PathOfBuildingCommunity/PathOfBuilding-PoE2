d8d4689480814fece9eb0856d7509b23c0240c2c
MrHB212 <h.barut97@gmail.com>
2026-01-23 16:10:51 +0100

    Update CalcOffence.lua

--- src/Modules/CalcOffence.lua
+++ src/Modules/CalcOffence.lua
@@ -2708,7 +2708,7 @@ function calcs.offence(env, actor, activeSkill)
 				output.Cooldown = globalOutput.Cooldown
 				output.Speed = m_min(output.Speed, 1 / output.Cooldown * output.Repeats)
 			end
-			if (output.Cooldown and skillFlags.selfCast) or (skillFlags.duration and skillFlags.duration) then
+			if (output.Cooldown and skillFlags.selfCast) or (SkillType.Sustained) then
 				skillFlags.notAverage = true
 				skillFlags.showAverage = false
 				skillData.showAverage = false
