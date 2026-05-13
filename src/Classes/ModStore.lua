19b44264df30cdaddb3325f9f444704ec43ab3e1
LocalIdentity <localidentity2@gmail.com>
2026-05-14 06:51:52 +1000

    Fix handling of player actor

--- src/Classes/ModStore.lua
+++ src/Classes/ModStore.lua
@@ -34,6 +34,14 @@ local ModStoreClass = newClass("ModStore", function(self, parent)
 	self.conditions = { }
 end)
 
+local function getActor(self, actorType)
+	if actorType == "player" then
+		return self.actor.player or (self.actor.parent and self.actor.parent.player) or (self.actor.enemy and self.actor.enemy.player)
+	else
+		return self.actor[actorType]
+	end
+end
+
 function ModStoreClass:ScaleAddMod(mod, scale)
 	local unscalable = false
 	for _, effects in ipairs(mod) do
@@ -315,15 +323,17 @@ function ModStoreClass:EvalMod(mod, cfg, globalLimits)
 			-- This explicit target is necessary because even though the GetMultiplier method does call self.parent.GetMultiplier, it does so with noMod = true,
 			-- disabling the summation (3rd part): (not noMod and self:Sum("BASE", cfg, multiplierName[var]) or 0)
 			if tag.limitActor then
-				if self.actor[tag.limitActor] then
-					limitTarget = self.actor[tag.limitActor].modDB
+				local limitActor = getActor(self, tag.limitActor)
+				if limitActor then
+					limitTarget = limitActor.modDB
 				else
 					return
 				end
 			end
 			if tag.actor then
-				if self.actor[tag.actor] then
-					target = self.actor[tag.actor].modDB
+				local actor = getActor(self, tag.actor)
+				if actor then
+					target = actor.modDB
 				else
 					return
 				end
@@ -387,15 +397,17 @@ function ModStoreClass:EvalMod(mod, cfg, globalLimits)
 			local target = self
 			local thresholdTarget = self
 			if tag.thresholdActor then
-				if self.actor[tag.thresholdActor] then
-					thresholdTarget = self.actor[tag.thresholdActor].modDB
+				local thresholdActor = getActor(self, tag.thresholdActor)
+				if thresholdActor then
+					thresholdTarget = thresholdActor.modDB
 				else
 					return
 				end
 			end
 			if tag.actor then
-				if self.actor[tag.actor] then
-					target = self.actor[tag.actor].modDB
+				local actor = getActor(self, tag.actor)
+				if actor then
+					target = actor.modDB
 				else
 					return
 				end
@@ -417,8 +429,9 @@ function ModStoreClass:EvalMod(mod, cfg, globalLimits)
 			local target = self
 			-- This functions similar to the above tagTypes in regard to which actor to use, but for PerStat
 			-- if the actor is 'parent', we don't want to return if we're already using 'parent', just keep using 'self'
-			if tag.actor and self.actor[tag.actor] then
-				target = self.actor[tag.actor].modDB
+			local actor = getActor(self, tag.actor)
+			if actor then
+				target = actor.modDB
 			end
 			if tag.statList then
 				base = 0
@@ -465,8 +478,9 @@ function ModStoreClass:EvalMod(mod, cfg, globalLimits)
 			local target = self
 			-- This functions similar to the above tagTypes in regard to which actor to use, but for PercentStat
 			-- if the actor is 'parent', we don't want to return if we're already using 'parent', just keep using 'self'
-			if tag.actor and self.actor[tag.actor] then
-				target = self.actor[tag.actor].modDB
+			local actor = getActor(self, tag.actor)
+			if actor then
+				target = actor.modDB
 			end
 			if tag.statList then
 				base = 0
@@ -581,7 +595,8 @@ function ModStoreClass:EvalMod(mod, cfg, globalLimits)
 			local match = false
 			local target = self
 			if tag.actor then
-				target = self.actor[tag.actor] and self.actor[tag.actor].modDB
+				local actor = getActor(self, tag.actor)
+				target = actor and actor.modDB
 			end
 			if target and (tag.var or tag.varList) then
 				if tag.varList then
