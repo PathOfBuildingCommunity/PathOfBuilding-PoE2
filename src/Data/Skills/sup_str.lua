a6a13717a971de206a8a84efda5e48d5c3065791
DragoonWraith (github: krryan) <dragoonwraith@gmail.com>
2026-05-17 01:20:50 -0400

    Fix Quill Burst damage calculation more/better?

--- src/Data/Skills/sup_str.lua
+++ src/Data/Skills/sup_str.lua
@@ -5250,6 +5250,11 @@ skills["TriggeredQuillburstPlayer"] = {
 			label = "Quill Burst",
 			incrementalEffectiveness = 0.054999999701977,
 			statDescriptionScope = "triggered_spiked_gauntlets",
+			statMap = {
+				["display_skill_deals_thorns_damage"] = {
+					flag("ThornsDamageAppliesToHits"),
+				},
+			},
 			baseFlags = {
 				thorns = true,
 			},
