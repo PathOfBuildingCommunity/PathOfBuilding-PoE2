0d8ed3dbfb3e8309b4281be64f95c19d585d1bfd
vaisest <4550061+vaisest@users.noreply.github.com>
2026-05-22 21:53:55 +0300

    Fix trade hash generation

--- src/Export/Scripts/mods.lua
+++ src/Export/Scripts/mods.lua
@@ -1,6 +1,7 @@
 if not loadStatFile then
 	dofile("statdesc.lua")
 end
+local statDescriptions = getStatDescriptors("stat_descriptions.csd")
 loadStatFile("stat_descriptions.csd")
 
 -- not comprehensive. see moddomains table in export tool and enums.lua
@@ -29,6 +30,21 @@ function table.containsId(table, element)
 		end
 	end
 	return false
+end
+
+-- used for calculating the hash field of a stat
+local GGG_STAT_HASH32_SEED = 0xC58F1A7B
+-- used for calculating the trade hash from stat hash fields
+local GGG_TRADE_SEED = 0x02312233
+---@param stats string[]
+---@return integer
+local function hashStats(stats)
+	local statHashes = ""
+	for _, statName in ipairs(stats) do
+		local newHash = intToBytes(murmurHash2(statName, GGG_STAT_HASH32_SEED))
+		statHashes = statHashes..newHash
+	end
+	return murmurHash2(statHashes, GGG_TRADE_SEED)
 end
 
 local whiteListStat = {
@@ -125,52 +141,68 @@ local function writeMods(outName, condFunc)
 					out:write('nodeType = ', mod.NodeType, ', ')
 				end
 
-				-- Note that some of the resulting hashes might not be correct.
-				-- Some of the trade hashes are also associated with another
-				-- value. For example the "only affects passives in # ring" is
-				-- correct here, but has a variant value appended to it like:
-				-- explicit.stat_3642528642|7. Some stats may also be handled in
-				-- a way similar to radius jewels that this script doesn't do.
-				local modIdx = 1
 				local tradeHashes = {}
-				while mod["Stat" .. modIdx] do
+				local statsHashed = {}
+				local isTinctureMod = (mod.Domain == Domains.Tincture) and
+					(mod.GenerationType == GenTypes.Prefix
+						or mod.GenerationType == GenTypes.Suffix)
+				for statIdx = 1, 6 do
 					local currentStats = {}
-					local stat = mod["Stat" .. modIdx]
-					currentStats[stat.Id] = {
-						min = mod["Stat" .. modIdx .. "Value"][1], max = mod["Stat" .. modIdx .. "Value"][2]
-					}
-					if modIdx == 6 then
+					local stat = mod["Stat" .. statIdx]
+					if not stat then
 						break
 					end
-					local bytes = intToBytes(stat.Hash)
-					-- # to # stats consist of two different stats as the min and max have different ranges
-					if stat.Id:match("minimum") then
-						local nextStat = mod["Stat" .. (modIdx + 1)]
-						if nextStat and nextStat.Id:match("maximum") then
-							modIdx = modIdx + 1
-							bytes = bytes .. intToBytes(nextStat.Hash)
-							currentStats[nextStat.Id] = {
-								min = mod["Stat" .. modIdx .. "Value"][1], max = mod["Stat" .. modIdx .. "Value"][2]
-							}
+					-- some stats are related to other stats, and should be
+					-- hashed with them. we don't want to hash e.g. the lower
+					-- and upper range of # to # damage modifiers separately.
+					if statsHashed[stat.Id] then
+						goto innercontinue
+					end
+
+					-- tincture stat descriptions are in a separate file
+					local statEntry
+					if isTinctureMod then
+						statEntry = tinctureStatDescriptions[stat.Id] and tinctureStatDescriptions[stat.Id]
+					else
+						statEntry = statDescriptions[stat.Id] and statDescriptions[stat.Id]
+					end
+
+					-- skip stats that are missing fields. these are most likely
+					-- hidden stats or e.g. map stats
+					if not statEntry or not statEntry.stats or not statEntry[1] then
+						goto innercontinue
+					end
+
+					-- match stats to the stat values on the mod and save them
+					-- as they're used to describe the stat
+					local currentStats = {}
+					for _, statId in ipairs(statEntry.stats) do
+						for statIdx = 1, 6 do
+							if mod["Stat" .. statIdx] and mod["Stat" .. statIdx].Id == statId then
+								currentStats[statId] = {
+									min = mod["Stat" .. statIdx .. "Value"][1],
+									max = mod["Stat" .. statIdx .. "Value"][2]
+								}
+							end
 						end
 					end
 
-					local description, _, _ = describeStats(currentStats)
-
+					local stats = copyTable(statEntry.stats)
 					-- radius jewel mods:
 					-- notable
 					if mod.NodeType == 2 then
-						-- append stat hash for
-						-- "local_jewel_mod_stats_added_to_notable_passives"
-						bytes = bytes .. intToBytes(1950420994)
-						-- small
+						table.insert(stats, "local_jewel_mod_stats_added_to_notable_passives")
+					-- small
 					elseif mod.NodeType and mod.NodeType == 1 then
-						-- append stat hash for
-						-- "local_jewel_mod_stats_added_to_small_passives"
-						bytes = bytes .. intToBytes(1498395485)
+						table.insert(stats, "local_jewel_mod_stats_added_to_small_passives")
 					end
-					tradeHashes[murmurHash2(bytes, 0x02312233)] = description
-					modIdx = modIdx + 1
+
+
+					local description, _, _ = describeStats(currentStats)
+
+					local tradeHash = hashStats(stats)
+					tradeHashes[tradeHash] = description
+					::innercontinue::
 				end
 				out:write("tradeHashes = { ")
 				for hash, desc in pairs(tradeHashes) do
