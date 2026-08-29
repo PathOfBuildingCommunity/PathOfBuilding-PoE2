8d9f9bf7ffe8e95ce8c7d92688be51536add1594
vaisest <vaisest@users.noreply.github.com>
2026-08-29 20:34:28 +0000

    Apply changes from https://github.com/PathOfBuildingCommunity/PathOfBuilding/pull/10120

--- src/Classes/ItemsTab.lua
+++ src/Classes/ItemsTab.lua
@@ -747,6 +747,7 @@ holding Shift will put it in the second.]])
 		end
 		return h
 	end})
+
 	for i = 1, maxModCount do
 		local prev = self.controls["displayItemAffix"..(i-1)] or self.controls.displayItemSectionAffix
 		local drop, slider
