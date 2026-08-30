38daa4dfd658f2539a8cc70d827fddedb8e30d70
mcagnion <mcagnion@users.noreply.github.com>
2026-08-30 11:02:03 +0000

    Apply changes from https://github.com/PathOfBuildingCommunity/PathOfBuilding/pull/10076

--- src/Classes/ItemsTab.lua
+++ src/Classes/ItemsTab.lua
@@ -3986,9 +3986,10 @@ function ItemsTabClass:AddItemTooltip(tooltip, item, slot, dbMode, maxWidth)
 		end
 	end
 	-- Stat differences
+	local itemTabHint = self.build.viewMode == "ITEMS" and "" or " in the Items tab"
 	if not self.showStatDifferences then
 		tooltip:AddSeparator(14)
-		tooltip:AddLine(14, colorCodes.TIP.."Tip: Press Ctrl+D to enable the display of stat differences.")
+		tooltip:AddLine(14, colorCodes.TIP.."Tip: Press Ctrl+D"..itemTabHint.." to enable the display of stat differences.")
 		return
 	end
 	local calcFunc, calcBase = self.build.calcsTab:GetMiscCalculator()
